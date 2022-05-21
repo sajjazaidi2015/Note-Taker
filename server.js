0f4e4587b5648f942e6661826a2aa474e7975dbe
Sajjad Zaidi <sajjadzaidi2015@gmail.com>
2022-05-20 21:10:14 -0400

    updating the post by unique id & creating the delete by unique id

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const path = require('path');
-const termData = require('./db/db.json')
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+const termData = require("./db/db.json");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,9 +11,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Middleware
-app.use(express.static('public'));
-
+// Middleware for parsing application/json and urlencoded data
+app.use(express.static("public"));
 
 // Three endpoints
 /*
@@ -24,23 +25,70 @@ app.use(express.static('public'));
 
 */
 
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/index.html"))
+);
 
-app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, 'public/index.html')))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
 
-app.get('/notes', (req, res) =>
-res.sendFile(path.join(__dirname, 'public/notes.html')))
+app.get("/api/notes", (req, res) => res.json(termData));
 
-app.get('/api/notes', (req, res) => res.json(termData))
+app.post("/api/notes", (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received`);
 
-app.post('/api/notes', (req, res) => {
-    res.json(`${req.method} request received`)
-    console.log(req.body)
-    console.log('Raw headers ===>', `${req.rawHeaders}`)
+  //Destructuring the notes in req.body
+  const { title, text } = req.body;
+  let newNote = { text: "", title: "", id: "" };
+  //if all the required properties are present
+  if (title && text) {
+    //Variable for the object we will save
+    newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+    const response = {
+      status: "success",
+      body: newNote,
+    };
+    console.log(response);
+    termData.push(newNote);
+  }
 
-    console.info(`${req.method} request received`)
+  fs.writeFile("./db/db.json", JSON.stringify(termData), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Your note was saved!");
+  });
+
+
+  // Gives back the response, which is the user's new note.
+  res.json(newNote);
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  console.log('params', req.params.id)
+  console.log(`${req.method} request received`)
+
+  const delNote = termData.filter (note => note.id !== req.params.id)
+  /*
+    get id from param
+    filter out the id you got from param to get an array that does not include
+  */
+  fs.writeFile("./db/db.json", JSON.stringify(delNote), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Your note was saved!");
+  });
+  res.sendStatus(200)
+    
 })
 
 app.listen(PORT, () =>
-console.log(`Example app listening at http://localhost:${PORT}`))
-
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
